f9a6b7cc0cd21f81789366aa6e99920594a29208
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-19 00:32:20 +0100

    feat: :sparkles: Add Genre Model

---
+++ models/genre.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const GenreSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxLength: 100,
+    minLength: 3,
+  },
+});
+
+GenreSchema.virtual("url").get(function () {
+  // eslint-disable-next-line no-underscore-dangle
+  return `/catalog/genre/${this._id}`;
+});
+
+module.exports = mongoose.model("Genre", GenreSchema);

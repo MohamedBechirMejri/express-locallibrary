97a9dfee179457ec7ffc81e9accfd05e7e9a6d59
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-19 00:32:52 +0100

    add eslint rule

--- models/genre.js
+++ models/genre.js
@@ -9,6 +9,7 @@ const GenreSchema = mongoose.Schema({
   },
 });
 
+// eslint-disable-next-line func-names
 GenreSchema.virtual("url").get(function () {
   // eslint-disable-next-line no-underscore-dangle
   return `/catalog/genre/${this._id}`;

d61a570e6588042cda50dfd547d13a50e68d00e4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-18 21:42:10 +0100

    feat: :sparkles: Add Author model

---
+++ models/author.js
@@ -0,0 +1,35 @@
+/* eslint-disable func-names */
+const mongoose = require("mongoose");
+
+const AuthorSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  family_name: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  date_of_birth: Date,
+  date_of_death: Date,
+});
+
+AuthorSchema.virtual("name").get(function () {
+  return this.family_name && this.first_name
+    ? `${this.family_name}, ${this.first_name}`
+    : "";
+});
+
+AuthorSchema.virtual("lifespan").get(function () {
+  return this.date_of_birth && this.date_of_death
+    ? `${this.date_of_birth.getFullYear()} - ${this.date_of_death.getFullYear()}`
+    : "";
+});
+AuthorSchema.virtual("url").get(function () {
+  // eslint-disable-next-line no-underscore-dangle
+  return `/catalog/author/${this._id}`;
+});
+
+module.exports = mongoose.model("Author", AuthorSchema);

abc9ab8d444f151aca8841d321bc9723cbc2fa13
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-19 01:05:34 +0100

    remove no underscore dangle rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier'
-  ],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    'no-console': 'warn',
-    'no-unused-vars': 'warn',
+    "no-console": "warn",
+    "no-unused-vars": "warn",
+    "no-underscore-dangle": "off",
   },
 };

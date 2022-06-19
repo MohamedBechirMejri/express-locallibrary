4bcb88081c24694516e56e045c8657689be9d8b4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-19 07:09:50 +0100

    switch to scss

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.use(
   sassMiddleware({
     src: path.join(__dirname, "public"),
     dest: path.join(__dirname, "public"),
-    indentedSyntax: true, // true = .sass and false = .scss
-    sourceMap: true,
+    indentedSyntax: false, // true = .sass and false = .scss
+    sourceMap: false,
   })
 );
 app.use(express.static(path.join(__dirname, "public")));

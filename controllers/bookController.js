06d0c0283224afbc8b8d8b12b7a6ca84f15d5420
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-21 04:02:30 +0100

    feat: :sparkles: Add index function

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,39 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-console */
+const async = require("async");
+
 const Book = require("../models/book");
+const Author = require("../models/author");
+const Genre = require("../models/genre");
+const BookInstance = require("../models/bookinstance");
 
 exports.index = (req, res) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+  async.parallel(
+    {
+      book_count() {
+        Book.countDocuments({});
+      },
+      book_instance_count() {
+        BookInstance.countDocuments({});
+      },
+      book_instance_available_count() {
+        BookInstance.countDocuments({ status: "Available" });
+      },
+      author_count() {
+        Author.countDocuments({});
+      },
+      genre_count() {
+        Genre.countDocuments({});
+      },
+    },
+    (err, results) => {
+      res.render("index", {
+        title: "Local Library Home",
+        error: err,
+        data: results,
+      });
+    }
+  );
 };
 
 // Display list of all books.

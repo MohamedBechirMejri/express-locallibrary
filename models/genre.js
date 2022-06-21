7f5e6f760de5d7956f528bb49829eaf1b99986a1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-21 07:20:20 +0100

    refactor: :fire: switch to arrow func

--- models/genre.js
+++ models/genre.js
@@ -1,18 +1,13 @@
 const mongoose = require("mongoose");
 
 const GenreSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    maxLength: 100,
-    minLength: 3,
-  },
+  name: { type: String, required: true, minLength: 3, maxLength: 100 },
 });
 
-// eslint-disable-next-line func-names
-GenreSchema.virtual("url").get(function () {
-  // eslint-disable-next-line no-underscore-dangle
-  return `/catalog/genre/${this._id}`;
-});
+GenreSchema.virtual("url").get(
+  () =>
+    // eslint-disable-next-line no-underscore-dangle
+    `/catalog/genre/${this._id}`
+);
 
 module.exports = mongoose.model("Genre", GenreSchema);

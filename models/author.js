f0412d9faf9e538acd8c93affcdaa5273aca634e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-23 05:29:23 +0100

    format dates

--- models/author.js
+++ models/author.js
@@ -1,5 +1,7 @@
+/* eslint-disable camelcase */
 /* eslint-disable func-names */
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon"); // for date handling
 
 const AuthorSchema = new mongoose.Schema({
   first_name: {
@@ -22,20 +24,37 @@ AuthorSchema.virtual("name").get(function () {
     : "";
 });
 
-AuthorSchema.virtual("lifespan").get(function () {
-  return this.date_of_birth && this.date_of_death
-    ? `${this.date_of_birth.getFullYear()} - ${this.date_of_death.getFullYear()}`
-    : "";
-});
+// AuthorSchema.virtual("lifespan").get(function () {
+//   return this.date_of_birth && this.date_of_death
+//     ? `${this.date_of_birth.getFullYear()} - ${this.date_of_death.getFullYear()}`
+//     : "";
+// });
 AuthorSchema.virtual("url").get(function () {
   // eslint-disable-next-line no-underscore-dangle
   return `/catalog/author/${this._id}`;
 });
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  return lifetime_string;
+});
 
-AuthorSchema.virtual("lifespan_formatted").get(function () {
-  return this.date_of_birth && this.date_of_death
-    ? `${this.date_of_birth.getFullYear()} - ${this.date_of_death.getFullYear()}`
-    : "";
+AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
+});
+
+AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);

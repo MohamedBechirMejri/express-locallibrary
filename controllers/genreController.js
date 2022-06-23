00b4a729d46ce4991bd1f71b9c7ae66e7f2991b1
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-06-23 05:18:00 +0100

    add genre update methods

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -169,11 +169,57 @@ exports.genre_delete_post = (req, res, next) => {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+exports.genre_update_get = function (req, res, next) {
+  Genre.findById(req.params.id, (err, genre) => {
+    if (err) {
+      return next(err);
+    }
+    if (genre == null) {
+      // No results.
+      const errr = new Error("Genre not found");
+      errr.status = 404;
+      return next(errr);
+    }
+    // Success.
+    res.render("genre_form", { title: "Update Genre", genre });
+  });
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-};
+exports.genre_update_post = [
+  // Validate and sanitze the name field.
+  body("name", "Genre name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request .
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data (and the old id!)
+    const genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values and error messages.
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre,
+        errors: errors.array(),
+      });
+    } else {
+      // Data from form is valid. Update the record.
+      Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre) => {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to genre detail page.
+        res.redirect(thegenre.url);
+      });
+    }
+  },
+];
